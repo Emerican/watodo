ea749f81c2c9c5c455a4262424672a9a4e89c115
Matīss Strautmanis <matiss.strautmanis@gmail.com>
2014-09-03 15:28:40 +0300

    backed up lecture.

--- watodo/src/com/vellameita/watodo/startingPoint.java
+++ watodo/src/com/vellameita/watodo/startingPoint.java
@@ -1,7 +1,7 @@
 package com.vellameita.watodo;
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
+//import android.content.Intent;
+//import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +9,7 @@ import android.widget.TextView;
  
 public class startingPoint extends Activity {
 	int counter;
-	Button link;
+	//Button link;
 	Button add;
 	Button sub;
 	TextView display;
@@ -19,7 +19,7 @@ public class startingPoint extends Activity {
 		setContentView(R.layout.main);
 		counter = 0;
 		
-		link = (Button) findViewById(R.id.bLink);
+	//link = (Button) findViewById(R.id.bLink);
 		add = (Button) findViewById(R.id.bAdd);
 		sub = (Button) findViewById(R.id.bSub);
 		display = (TextView) findViewById(R.id.tvDisplay);
@@ -35,13 +35,13 @@ public class startingPoint extends Activity {
 				display.setText("You still have raped " + counter + " pokemon");
 			}
 		});		
-		link.setOnClickListener(new View.OnClickListener(){
+	/*	link.setOnClickListener(new View.OnClickListener(){
 		public void onClick(View v) {
 		Uri uri = Uri.parse("http://www.strautmanis.lv");
 		        Intent callLink = new Intent(Intent.ACTION_VIEW, uri);
 		       startActivity(callLink); 
 			}
-		});
+		});*/
 		
 	}
 }
